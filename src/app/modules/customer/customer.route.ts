81dc4da484d5aa9a2073036986f7e2dd850623aa
Th3At0nic <islammdrahatul@gmail.com>
2025-05-09 18:25:35 +0600

    bike service done

--- src/app/modules/customer/customer.route.ts
+++ src/app/modules/customer/customer.route.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import { CustomerController } from "./customer.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { CustomerValidations } from "./customer.validation";
 
 const router = express.Router();
 
-router.post("/", CustomerController.createCustomer);
+router.post(
+  "/",
+  validateRequest(CustomerValidations.createCustomerSchema),
+  CustomerController.createCustomer
+);
 router.get("/", CustomerController.getAllCustomers);
 router.get("/:id", CustomerController.getCustomerById);
-router.put("/:id", CustomerController.updateCustomer);
+router.put(
+  "/:id",
+  validateRequest(CustomerValidations.updateCustomerSchema),
+  CustomerController.updateCustomer
+);
 router.delete("/:id", CustomerController.deleteCustomer);
 
 export const CustomerRoutes = router;
