801b491eb68f19594f85677edb90df077e23ede9
Th3At0nic <islammdrahatul@gmai.com>
2025-05-09 23:11:42 +0600

    serivce module done, bonus section done, solved the route issue of service route(dynamic route shouldbe at least down)

--- src/app/modules/serviceRecord/serviceRecord.route.ts
+++ src/app/modules/serviceRecord/serviceRecord.route.ts
@@ -10,9 +10,13 @@ router.post(
   validateRequest(ServiceRecordValidations.createServiceRecordSchema),
   ServiceControllers.createService
 );
-router.get("/", ServiceControllers.getAllServices);
-router.get("/:id", ServiceControllers.getOneService);
-router.put("/:id/complete", ServiceControllers.completeServiceRecord);
 router.get("/status", ServiceControllers.getPendingOrOverdueServices);
+
+router.get("/", ServiceControllers.getAllServices);
+router.put("/:id/complete", ServiceControllers.completeServiceRecord);
+
+//this kind of dynamic route should be at the most down, because express check the routese top-down,
+// so other string can be assumes as dynamic route which creates wrong controller call
+router.get("/:id", ServiceControllers.getOneService);
 
 export const ServiceRecordRoutes = router;
