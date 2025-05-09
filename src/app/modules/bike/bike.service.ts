22ceca8635a68883facca0bdfcd4efc67bb70ca7
Th3At0nic <islammdrahatul@gmail.com>
2025-05-09 18:05:44 +0600

    modified customer service file

---
+++ src/app/modules/bike/bike.service.ts
@@ -0,0 +1,5 @@
+
+
+const createBikeIntoDb = async( payload: any) => {
+    
+}
--- src/app/modules/customer/customer.service.ts
+++ src/app/modules/customer/customer.service.ts
@@ -1,25 +1,69 @@
 import { PrismaClient } from "@prisma/client";
+import throwAppError from "../../utils/throwAppError";
+import { StatusCodes } from "http-status-codes";
 
 const prisma = new PrismaClient();
 
 const createCustomerIntoDb = async (payload: any) => {
+  const existingCustomer = await prisma.customer.findUnique({
+    where: { email: payload.email },
+  });
+
+  if (existingCustomer) {
+    throwAppError(
+      "email",
+      "Customer with this email is already exists",
+      StatusCodes.CONFLICT
+    );
+  }
   return prisma.customer.create({ data: payload });
 };
 
 const getAllCustomersFromDb = async () => {
-  return prisma.customer.findMany();
+  const allCustomers = await prisma.customer.findMany();
+  if (!allCustomers.length) {
+    throwAppError("", "No Customer Found", StatusCodes.NOT_FOUND);
+  }
+  return allCustomers;
 };
 
 const getCustomerByIdFromDB = async (customerId: string) => {
-  return prisma.customer.findUnique({ where: { customerId } });
+  const customer = await prisma.customer.findUnique({ where: { customerId } });
+  if (!customer) {
+    throwAppError(
+      "customerId",
+      "Customer Not Found with the id",
+      StatusCodes.NOT_FOUND
+    );
+  }
+  return customer;
 };
 
 const updateCustomerIntoDb = async (customerId: string, payload: any) => {
-  return prisma.customer.update({ where: { customerId }, data: payload });
+  const updatedCustomer = await prisma.customer.update({
+    where: { customerId },
+    data: payload,
+  });
+  if (!updatedCustomer) {
+    throwAppError(
+      "customerId",
+      "Couldn't Update. Customer Not Found with the id or Something went wrong",
+      StatusCodes.NOT_FOUND
+    );
+  }
+  return updatedCustomer;
 };
 
 const deleteCustomerFromDb = async (customerId: string) => {
-  return prisma.customer.delete({ where: { customerId } });
+  const result = await prisma.customer.delete({ where: { customerId } });
+  if (!result) {
+    throwAppError(
+      "customerId",
+      "Couldn't Delete. Customer Not Found with the id or Something went wrong",
+      StatusCodes.NOT_FOUND
+    );
+  }
+  return result;
 };
 
 export const CustomerServices = {
